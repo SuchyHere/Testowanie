a92d687ed07915b3f18dffa6be399a28a93caf06
olohiperbolo <aleksanderopszala@gmail.com>
2024-05-07 09:57:50 +0200

    pozdr

--- locustfile.py
+++ locustfile.py
@@ -4,7 +4,7 @@ from locust import HttpUser, task, between
 class UserBehavior(HttpUser):
     wait_time = between(1, 5)
 
-    @task(3)
+    @task(1)
     def view_posts(self):
         self.client.get("/posts")
 
@@ -12,6 +12,14 @@ class UserBehavior(HttpUser):
     def view_albums(self):
         self.client.get("/albums")
 
-    @task(1)
+    @task(3)
     def view_photos(self):
         self.client.get("/photos")
+
+    @task(4)
+    def view_limit_albums(self):
+        self.client.get("/albums?limit=5")
+
+    @task(5)
+    def view_limit_posts(self):
+        self.client.get("/posts?limit=10")
